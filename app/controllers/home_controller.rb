3f9aa950e2dff4184df669ad985e77d5669196dd
Piotr Wasiak <chooosen@gmail.com>
2017-01-10 15:28:57 +0100

    Add User model and device configuration

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+# Welcome page controller
 class HomeController < ApplicationController
   def index; end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+# Model for logging trainer and player
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
