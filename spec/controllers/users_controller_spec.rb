b64e8d5735f693f34da81b5b9a5170dce7cfd85c
Piotr Wasiak <chooosen@gmail.com>
2017-01-11 09:46:57 +0100

    Fix users tests and add view

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { FactoryGirl.create(:user) }
+
   describe 'GET #show' do
     it 'returns http success' do
-      get :show
+      get :show, params: { id: user.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -11,6 +13,13 @@ RSpec.describe UsersController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'returns http success' do
+      get :edit, params: { id: user.id }
       expect(response).to have_http_status(:success)
     end
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 FactoryGirl.define do
   factory :user do
+    email 'tester@wp.pl'
+    password 'password123'
   end
 end
