f283c7cd3ab207d2cf38101afd0796446ca738e9
Piotr Wasiak <chooosen@gmail.com>
2017-01-24 17:25:01 +0100

    Add training specs and improve factories

--- spec/factories/trainings.rb
+++ spec/factories/trainings.rb
@@ -2,5 +2,27 @@ FactoryGirl.define do
   factory :training do
     date DateTime.current + 2.days
     price '120.00'
+
+    factory :training_with_user do
+      ignore do
+        user { create(:user) }
+      end
+
+      after(:create) do |training, evaluator|
+        training.users << evaluator.user
+      end
+    end
+
+    factory :training_with_users do
+      ignore do
+        users_count 3
+      end
+
+      after(:create) do |training, evaluator|
+        (0...evaluator.users_count).each do
+          training.users << create(:user)
+        end
+      end
+    end
   end
 end
--- spec/models/training_spec.rb
+++ spec/models/training_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe Training, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Training, type: :model do
+  let(:training) { create(:training) }
+  subject { training }
+
+  it 'is invalid without a date' do
+    training.date = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is invalid with negative price' do
+    training.price = -20.50
+    expect(subject).not_to be_valid
+  end
+
+  it 'is valid with no price' do
+    training.price = nil
+    expect(subject).to be_valid
+  end
+
+  describe '#price_per_user' do
+    context 'when there are no users_trainings' do
+      let(:training) { create(:training) }
+      subject { training.price_per_user }
+      it 'returns 0 if there are no users in training' do
+        expect(subject).to eq 0
+      end
+    end
+
+    context 'when there are 3 users_trainings' do
+      context 'when 2 of them attended' do
+        let(:training) { create(:training_with_users) }
+        subject { training.price_per_user }
+
+        it 'has 2 users' do
+          expect(training.users.count).to eq 2
+        end
+
+        it 'has 2 users' do
+          expect(training.users_trainings.count).to eq 2
+        end
+
+        it 'devides price by 2' do
+          expect(subject).to eq training.price / 2
+        end
+      end
+    end
+  end
 end
