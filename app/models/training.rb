ad61db3c33ff9adff6ec35477a11a9579c31f0fd
Piotr Wasiak <chooosen@gmail.com>
2017-01-24 17:18:27 +0100

    Validate training price

--- app/models/training.rb
+++ app/models/training.rb
@@ -4,6 +4,7 @@ class Training < ApplicationRecord
   has_many :users_trainings, dependent: :destroy
 
   validates :date, presence: true
+  validates :price, allow_nil: true, numericality: { greater_than: 0.0 }
 
   scope :date_between,
         ->(start, end_d) { where('date >= ? AND date <= ?', start, end_d) }
