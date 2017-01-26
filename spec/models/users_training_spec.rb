aa25398f9904e82a141d0ae2bb68600619b2dd74
Piotr Wasiak <chooosen@gmail.com>
2017-01-26 14:21:14 +0100

    Add custom validations specs to users_trainings

--- spec/models/users_training_spec.rb
+++ spec/models/users_training_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersTraining, type: :model do
   let(:today) { Time.zone.today }
-  let(:users_training) { create(:users_training) }
+  let(:users_training) { build(:users_training) }
 
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :training }
 
+  subject { users_training }
+
   describe 'custom validations:' do
     describe 'change of accepted_at' do
-      subject { users_training }
-
       context 'when accepted_at is set' do
         let(:users_training) { create(:users_training, accepted_at: today) }
 
         it 'is not valid' do
           expect { subject.accepted_at += 1.day }.to change { subject.valid? }.
-            from(true).to(false)
+            from(true).to false
         end
       end
 
@@ -28,11 +28,34 @@ RSpec.describe UsersTraining, type: :model do
       end
     end
 
-    describe 'set accepted_at only before training' do
-      skip('#TODO')
+    describe 'set accepted_at' do
+      context 'when training end date is in the past' do
+        it 'is not valid' do
+          expect { subject.training.date = today - 1.month }.
+            to change { subject.valid? }.from(true).to false
+        end
+      end
+
+      context 'when training end date is in the future' do
+        it 'is valid' do
+          expect(subject.valid?).to eq true
+        end
+      end
     end
-    describe 'send invitition before training date' do
-      skip('#TODO')
+
+    describe 'Invite user to training' do
+      context 'when training is in the future' do
+        it 'is valid' do
+          expect(subject.valid?).to eq true
+        end
+      end
+
+      context 'when training is in the past' do
+        it 'is not valid' do
+          expect { subject.training.date = today - 1.month }.
+            to change { subject.valid? }.from(true).to(false)
+        end
+      end
     end
   end
 end
