e7fb9a916dc562c481f7d1a157df7d98305b6dde
Piotr Wasiak <chooosen@gmail.com>
2017-01-26 12:35:47 +0100

    Add validator test: accepted_at_not_changed

--- spec/models/users_training_spec.rb
+++ spec/models/users_training_spec.rb
@@ -1,6 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe UsersTraining, type: :model do
+  let(:today) { Time.zone.today }
+  let(:users_training) { create(:users_training) }
+
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :training }
+
+  describe 'custom validations:' do
+    describe 'change of accepted_at' do
+      subject { users_training }
+
+      context 'when accepted_at is set' do
+        let(:users_training) { create(:users_training, accepted_at: today) }
+
+        it 'is not valid' do
+          expect { subject.accepted_at += 1.day }.to change { subject.valid? }.
+            from(true).to(false)
+        end
+      end
+
+      context 'when accepted_at is nil' do
+        it 'is valid' do
+          expect { subject.accepted_at = today }.
+            not_to change { subject.valid? }
+        end
+      end
+    end
+
+    describe 'set accepted_at only before training' do
+      skip('#TODO')
+    end
+    describe 'send invitition before training date' do
+      skip('#TODO')
+    end
+  end
 end
