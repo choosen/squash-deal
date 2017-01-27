e07079468e2aeea6e6316e353c72a7e9d1b06108
Piotr Wasiak <chooosen@gmail.com>
2017-01-27 11:09:47 +0100

    Add User before save name update and #display_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 # Model for logging trainer and player
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save :set_name_if_blank
+
   devise :invitable, :database_authenticatable, :registerable,
          :confirmable, :recoverable, :rememberable, :trackable, :validatable
   has_many :trainings, through: :users_trainings
   has_many :users_trainings, dependent: :destroy
+
+  def set_name_if_blank
+    self.name = email.split('@').first if name.blank?
+  end
+
+  def display_name
+    name.blank? ? email : name
+  end
 end
