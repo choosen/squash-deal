7e1f195385413b6d4adba75614285aa00026d193
Piotr Wasiak <chooosen@gmail.com>
2017-01-30 12:09:29 +0100

    Set default timezone and formats for Date and Time

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Squashapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Europe/Warsaw'
+    config.active_record.default_timezone = :local # Or :utc
+
     config.generators do |g|
       g.test_framework :rspec, views: true, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
