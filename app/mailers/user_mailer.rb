138a18477c324a2c625134112b1dddeda4c40fc7
Piotr Wasiak <chooosen@gmail.com>
2017-01-30 13:17:03 +0100

    Refactor emails to passing formated Training.date

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,23 +4,25 @@ class UserMailer < ApplicationMailer
 
   def training_invitation(users_training)
     @user = users_training.user
-    @training = users_training.training
-    @decline_url = training_invitation_remove_url(@training)
-    @accept_url = training_invitation_accept_url(@training)
+    training = users_training.training
+    @decline_url = training_invitation_remove_url(training)
+    @accept_url = training_invitation_accept_url(training)
+    @training_date = I18n.l training.date
     mail(to: @user.email, subject: 'Squash training invitation')
   end
 
   def payment_reminder(users_training, training)
     @user = users_training.user
-    @training = training
+    @training_date = I18n.l training.date
     @training_user_price = users_training.user_price
     mail(to: @user.email, subject: 'Squash training payment reminder')
   end
 
   def training_reminder(users_training)
     @user = users_training.user
-    @training = users_training.training
-    training_at = I18n.l @training.date, format: :time
+    training = users_training.training
+    training_at = I18n.l training.date, format: :time
+    @training_date = I18n.l training.date
     mail(to: @user.email,
          subject: "Your Squash training is tomorrow at #{training_at}")
   end
