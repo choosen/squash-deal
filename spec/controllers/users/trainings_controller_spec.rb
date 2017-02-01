8a66f214694434d1c6ef1320b22304514e9790eb
Piotr Wasiak <chooosen@gmail.com>
2017-02-01 15:24:07 +0100

    Spec trainings#update on multisport_used

--- spec/controllers/users/trainings_controller_spec.rb
+++ spec/controllers/users/trainings_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Users::TrainingsController, type: :controller do
     let(:valid_params) do
       users_training.attributes.slice('user_id', 'training_id').symbolize_keys
     end
-    let(:invalid_params) { valid_params.slice(:user_id) }
+    let(:wrong_params) { valid_params.slice(:user_id) }
 
     context 'with valid params' do
       subject do
@@ -44,7 +44,7 @@ RSpec.describe Users::TrainingsController, type: :controller do
     context 'with invalid params' do
       subject do
         post :create, params: { user_id: valid_params[:user_id],
-                                users_training: invalid_params }
+                                users_training: wrong_params }
       end
 
       it "doesn't create a new Training" do
@@ -61,59 +61,64 @@ RSpec.describe Users::TrainingsController, type: :controller do
 
   describe 'PUT #update' do
     let(:users_training) { FactoryGirl.create(:users_training) }
-    let(:valid_params) do
-      users_training.attributes.slice('user_id').
-        symbolize_keys.merge(attended: true, id: users_training.training.id)
-    end
-    let(:invalid_params) { valid_params.slice(:user_id).merge(id: -5) }
-
-    context 'with valid params' do
-      subject { put :update, params: valid_params }
-
-      context 'when users_training is not marked as attended' do
-        it 'updates the users_training' do
-          expect { subject }.to change { users_training.reload.attended }
-        end
-
-        it 'redirects to the training and flash success' do
-          subject
-          expect(response).to redirect_to(UsersTraining.last.training)
-          expect(flash[:success]).to eq 'User training info updated'
-        end
-      end
-
-      context 'when users_training is marked as attended' do
-        let(:ut_attended) do
-          FactoryGirl.create(:users_training, attended: true)
-        end
+    [:attended, :multisport_used].each do |field|
+      context "with #{field} in params" do
         let(:valid_params) do
-          ut_attended.attributes.slice('user_id').symbolize_keys.
-            merge(attended: false, id: ut_attended.training.id)
+          users_training.attributes.slice('user_id').
+            symbolize_keys.merge(field => true, id: users_training.training.id)
+        end
+        let(:wrong_params) { valid_params.slice(:user_id, field).merge(id: 0) }
+
+        context 'with valid params' do
+          subject { put :update, params: valid_params }
+
+          context "when users_training is not marked as #{field}" do
+            it 'updates the users_training' do
+              expect { subject }.to change { users_training.reload.send(field) }
+            end
+
+            it 'redirects to the training and flash success' do
+              subject
+              expect(response).to redirect_to(UsersTraining.last.training)
+              expect(flash[:success]).to eq 'User training info updated'
+            end
+          end
+
+          context "when users_training is marked as #{field}" do
+            let(:ut_attended) do
+              FactoryGirl.create(:users_training, field => true)
+            end
+            let(:valid_params) do
+              ut_attended.attributes.slice('user_id').symbolize_keys.
+                merge(field => false, id: ut_attended.training.id)
+            end
+
+            it 'updates the users_training' do
+              expect { subject }.to change { ut_attended.reload.send(field) }
+            end
+
+            it 'redirects to the training and flash success' do
+              subject
+              expect(flash[:success]).to eq 'User training info updated'
+              expect(response).to redirect_to(ut_attended.training)
+            end
+          end
         end
 
-        it 'updates the users_training' do
-          expect { subject }.to change { ut_attended.reload.attended }
+        context 'with invalid params' do
+          subject { put :update, params: wrong_params }
+
+          it "doesn't change users_training" do
+            expect { subject }.
+              not_to change { users_training.reload.send(field) }
+          end
+
+          it 'redirects to the root with flash' do
+            subject
+            expect(flash[:error]).to eq 'Error during update'
+            expect(response).to redirect_to(root_path)
+          end
         end
-
-        it 'redirects to the training and flash success' do
-          subject
-          expect(flash[:success]).to eq 'User training info updated'
-          expect(response).to redirect_to(ut_attended.training)
-        end
-      end
-    end
-
-    context 'with invalid params' do
-      subject { put :update, params: invalid_params }
-
-      it "doesn't change users_training" do
-        expect { subject }.not_to change { users_training.reload.attended }
-      end
-
-      it 'redirects to the root with flash' do
-        subject
-        expect(flash[:error]).to eq 'Error during update'
-        expect(response).to redirect_to(root_path)
       end
     end
   end
