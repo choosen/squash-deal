b86f4755c417854c2623e0bf7ba49e7f2c086d13
Piotr Wasiak <chooosen@gmail.com>
2017-02-02 13:55:17 +0100

    Upgrade schema version

--- db/migrate/20170130132257_add_owner_to_training.rb
+++ db/migrate/20170130132257_add_owner_to_training.rb
@@ -1,7 +1,7 @@
 class AddOwnerToTraining < ActiveRecord::Migration[5.0]
   def change
     add_reference :trainings, :owner, foreign_key: { to_table: :users }
-    reversible do |dir|
+    reversible do |dir| # Set first traiing user as default owner
       dir.up { Training.all.each { |t| t.update(owner: t.users.first || User.first) }  }
     end
   end
