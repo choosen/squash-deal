a90c055d7e02e61ed7485c684ff56e11a221112c
Piotr Wasiak <chooosen@gmail.com>
2017-02-03 14:33:32 +0100

    Secure migration by defining used ActiveRecord classes

--- db/migrate/20170130132257_add_owner_to_training.rb
+++ db/migrate/20170130132257_add_owner_to_training.rb
@@ -1,4 +1,19 @@
 class AddOwnerToTraining < ActiveRecord::Migration[5.0]
+  class Training < ActiveRecord::Base
+    has_many :users, through: :users_trainings
+    has_many :users_trainings
+    belongs_to :owner, class_name: 'User'
+  end
+
+  class UsersTraining < ActiveRecord::Base
+    self.primary_keys = :user_id, :training_id
+    belongs_to :user
+    belongs_to :training
+  end
+
+  class User < ActiveRecord::Base
+  end
+
   def change
     add_reference :trainings, :owner, foreign_key: { to_table: :users }
     reversible do |dir| # Set first traiing user as default owner
