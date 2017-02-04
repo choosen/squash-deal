ebb36392abd52163ed46fd60f8985e5a8f30c8e0
Piotr Wasiak <chooosen@gmail.com>
2017-02-04 14:39:41 +0100

    Omniauth2 initialize

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :confirmable, :recoverable, :rememberable, :trackable, :validatable
+         :confirmable, :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
   has_many :trainings, through: :users_trainings
   has_many :users_trainings, dependent: :destroy
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.find_by(email: data['email'])
+
+    # users to be created if they don't exist
+    unless user
+      user = User.create(name: data['name'],
+                         email: data['email'],
+                         password: Devise.friendly_token[0, 20])
+    end
+    user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'home#index'
+
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :users, only: [:index, :show, :edit, :update] do
     resources :trainings, controller: 'users/trainings', only: [:index, :create, :update]
