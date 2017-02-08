35c8ce95481bd04bd91f14432f0bf892630acfb6
Piotr Wasiak <chooosen@gmail.com>
2017-02-08 11:32:24 +0100

    Restrict permissions: work on finished

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,10 +27,10 @@ class Ability
     can :read, :all
     can :manage, User, id: user.id
     can :create, Training
-    can :manage, Training, owner_id: user.id
+    can :manage, Training, owner_id: user.id, finished?: false
     can :update, UsersTraining, training: { owner_id: user.id, done?: true }
     can :update, UsersTraining, user_id: user.id, training: { done?: false }
-    can :reaction_to_invite, Training
+    can :reaction_to_invite, Training, finished?: false
     can :reaction_to_invite, UsersTraining, user_id: user.id
   end
 end
