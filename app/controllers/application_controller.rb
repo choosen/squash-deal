7cd2f79066f889da43d90224a01c77a033f48d5e
Piotr Wasiak <chooosen@gmail.com>
2017-02-14 17:33:24 +0100

    Remove unnecessary links && small imnprovements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    respond_to do |format|
-      format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
-      format.js   { head :forbidden, content_type: 'text/html' }
-    end
-  end
-
-  rescue_from ActiveRecord::RecordNotFound do |exception|
+  rescue_from CanCan::AccessDenied, ActiveRecord::RecordNotFound do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
       format.html { redirect_to main_app.root_url, notice: exception.message }
